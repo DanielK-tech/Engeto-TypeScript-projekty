8eba16a81fb7dedc2481c976abf61b3c7af6a17d
DanielK-tech <dancek.kolegar@gmail.com>
2024-10-07 14:47:32 +0200

    little up

--- dist/script.js
+++ dist/script.js
@@ -1,9 +1,13 @@
 "use strict";
 const tlacitko = document.getElementById('button');
 const kostka = document.getElementById('Cube');
+const vysledky = document.getElementById('results');
 const hodKostkou = () => {
     const hozeneCislo = Math.floor(Math.random() * 6) + 1;
     kostka.src = `./img/${hozeneCislo}.jpg`;
+    const vysledek = document.createElement('p');
+    vysledek.textContent = `Hodili jste číslo: ${hozeneCislo}`;
+    vysledky.appendChild(vysledek);
 };
 if (tlacitko && kostka) {
     tlacitko.addEventListener('click', hodKostkou);
--- src/script.ts
+++ src/script.ts
@@ -1,10 +1,15 @@
 const tlacitko = document.getElementById('button') as HTMLButtonElement; 
 const kostka = document.getElementById('Cube') as HTMLImageElement; 
+const vysledky = document.getElementById('results') as HTMLElement;
 
 /*** fce na hod kostkou ***/ 
 const hodKostkou = () => { 
     const hozeneCislo = Math.floor(Math.random() * 6) + 1; 
-    kostka.src = `./img/${hozeneCislo}.jpg`;
+    kostka.src = `./img/${hozeneCislo}.jpg`; 
+    /** výsledky **/ 
+    const vysledek = document.createElement('p');
+    vysledek.textContent = `Hodili jste číslo: ${hozeneCislo}`;
+    vysledky.appendChild(vysledek);
 }
 
 if (tlacitko && kostka){ 
