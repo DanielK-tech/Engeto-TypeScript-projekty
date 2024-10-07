ff1755fdf8f09f4826f2ad05237a472c5e81bd71
DanielK-tech <dancek.kolegar@gmail.com>
2024-10-07 15:30:08 +0200

    little up

--- dist/script.js
+++ dist/script.js
@@ -1,14 +1,27 @@
 "use strict";
 const tlacitko = document.getElementById('button');
 const kostka = document.getElementById('Cube');
+const vynuluj = document.getElementById('zero');
 const vysledky = document.getElementById('results');
+const pocetHodu = document.getElementById('counting');
+const pocetBodu = document.getElementById('try');
+let soucet = 0;
+let pocetHozeni = 0;
 const hodKostkou = () => {
     const hozeneCislo = Math.floor(Math.random() * 6) + 1;
     kostka.src = `./img/${hozeneCislo}.jpg`;
     const vysledek = document.createElement('p');
     vysledek.textContent = `Hodili jste číslo: ${hozeneCislo}`;
     vysledky.appendChild(vysledek);
+    soucet += hozeneCislo;
+    pocetHozeni += 1;
+    pocetHodu.textContent = `Hodili jste: ${soucet}`;
+    pocetBodu.textContent = `Počet hodů: ${pocetHozeni}`;
 };
-if (tlacitko && kostka) {
+const vynulujVysledek = () => {
+    location.reload();
+};
+if (tlacitko && vynuluj) {
     tlacitko.addEventListener('click', hodKostkou);
+    vynuluj.addEventListener('click', vynulujVysledek);
 }
--- src/script.ts
+++ src/script.ts
@@ -1,17 +1,34 @@
 const tlacitko = document.getElementById('button') as HTMLButtonElement; 
 const kostka = document.getElementById('Cube') as HTMLImageElement; 
-const vysledky = document.getElementById('results') as HTMLElement;
+const vynuluj = document.getElementById('zero') as HTMLButtonElement;
+const vysledky = document.getElementById('results') as HTMLElement;  
+const pocetHodu = document.getElementById('counting') as HTMLParagraphElement; 
+const pocetBodu = document.getElementById('try') as HTMLParagraphElement;
+
+let soucet: number = 0; 
+let pocetHozeni: number = 0;
 
 /*** fce na hod kostkou ***/ 
-const hodKostkou = () => { 
-    const hozeneCislo = Math.floor(Math.random() * 6) + 1; 
-    kostka.src = `./img/${hozeneCislo}.jpg`; 
+const hodKostkou = (): number | void => { 
+    const hozeneCislo: number = Math.floor(Math.random() * 6) + 1; 
+    kostka.src = `./img/${hozeneCislo}.jpg`;      
     /** výsledky **/ 
     const vysledek = document.createElement('p');
     vysledek.textContent = `Hodili jste číslo: ${hozeneCislo}`;
-    vysledky.appendChild(vysledek);
+    vysledky.appendChild(vysledek); 
+    /** sčítání **/ 
+   soucet += hozeneCislo;  
+   pocetHozeni += 1;
+   /**Hodili jste: */ 
+   pocetHodu.textContent = `Hodili jste: ${soucet}`; 
+   pocetBodu.textContent = `Počet hodů: ${pocetHozeni}`;
+} 
+
+const vynulujVysledek = () => { 
+    location.reload();
 }
 
-if (tlacitko && kostka){ 
-    tlacitko.addEventListener('click', hodKostkou)
+if (tlacitko && vynuluj){ 
+    tlacitko.addEventListener('click', hodKostkou); 
+    vynuluj.addEventListener('click', vynulujVysledek);
 }
