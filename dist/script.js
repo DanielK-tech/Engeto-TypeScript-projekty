7be3494658c25dd76b7fe20371823ae0f335140d
Daniel K-tech <dancek.kolegar@gmail.com>
2024-10-08 18:06:34 +0200

    zapiname pasy

--- dist/script.js
+++ dist/script.js
@@ -1,17 +1,17 @@
 "use strict";
-const tlacitko = document.getElementById('button');
-const kostka = document.getElementById('Cube');
-const vynuluj = document.getElementById('zero');
-const vysledky = document.getElementById('results');
-const pocetHodu = document.getElementById('counting');
-const pocetBodu = document.getElementById('try');
+const tlacitko = document.getElementById("button");
+const kostka = document.getElementById("Cube");
+const vynuluj = document.getElementById("zero");
+const vysledky = document.getElementById("results");
+const pocetHodu = document.getElementById("counting");
+const pocetBodu = document.getElementById("try");
 let soucet = 0;
 let pocetHozeni = 0;
 const hodKostkou = () => {
     const hozeneCislo = Math.floor(Math.random() * 6) + 1;
     kostka.src = `./img/${hozeneCislo}.jpg`;
-    const vysledek = document.createElement('p');
-    vysledek.classList.add('temporary');
+    const vysledek = document.createElement("p");
+    vysledek.classList.add("temporary");
     vysledek.textContent = `Hodili jste číslo: ${hozeneCislo}`;
     vysledky.appendChild(vysledek);
     soucet += hozeneCislo;
@@ -19,17 +19,24 @@ const hodKostkou = () => {
     pocetHodu.textContent = `Hodili jste: ${soucet}`;
     pocetBodu.textContent = `Počet hodů: ${pocetHozeni}`;
     if (pocetHozeni <= 4 && soucet >= 18) {
-        const vyhra = document.createElement('h2');
-        vyhra.textContent = 'Vyhrál jsi!!';
+        const vyhra = document.createElement("h2");
+        vyhra.textContent = "Vyhrál jsi!!";
         vysledky.appendChild(vyhra);
-        const deleteTemporary = document.querySelectorAll('.temporary');
-        deleteTemporary.forEach(element => element.remove());
+        const deleteTemporary = document.querySelectorAll(".temporary");
+        deleteTemporary.forEach((element) => element.remove());
+    }
+    else if (pocetHozeni === 4 && soucet < 18) {
+        const prohra = document.createElement("h2");
+        prohra.textContent = "Prohrál jsi!!";
+        vysledky.appendChild(prohra);
+        const deleteTemporary = document.querySelectorAll(".temporary");
+        deleteTemporary.forEach((element) => element.remove());
     }
 };
 const vynulujVysledek = () => {
     location.reload();
 };
 if (tlacitko && vynuluj) {
-    tlacitko.addEventListener('click', hodKostkou);
-    vynuluj.addEventListener('click', vynulujVysledek);
+    tlacitko.addEventListener("click", hodKostkou);
+    vynuluj.addEventListener("click", vynulujVysledek);
 }
--- src/script.ts
+++ src/script.ts
@@ -1,45 +1,53 @@
-const tlacitko = document.getElementById('button') as HTMLButtonElement; 
-const kostka = document.getElementById('Cube') as HTMLImageElement; 
-const vynuluj = document.getElementById('zero') as HTMLButtonElement;
-const vysledky = document.getElementById('results') as HTMLElement;  
-const pocetHodu = document.getElementById('counting') as HTMLParagraphElement; 
-const pocetBodu = document.getElementById('try') as HTMLParagraphElement;
+const tlacitko = document.getElementById("button") as HTMLButtonElement;
+const kostka = document.getElementById("Cube") as HTMLImageElement;
+const vynuluj = document.getElementById("zero") as HTMLButtonElement;
+const vysledky = document.getElementById("results") as HTMLElement;
+const pocetHodu = document.getElementById("counting") as HTMLParagraphElement;
+const pocetBodu = document.getElementById("try") as HTMLParagraphElement;
 
-let soucet: number = 0; 
+let soucet: number = 0;
 let pocetHozeni: number = 0;
 
-/*** fce na hod kostkou ***/ 
-const hodKostkou = (): number | void => { 
-    const hozeneCislo: number = Math.floor(Math.random() * 6) + 1; 
-    kostka.src = `./img/${hozeneCislo}.jpg`;      
-    /** výsledky **/ 
-    const vysledek = document.createElement('p');
-    vysledek.classList.add('temporary');
-    vysledek.textContent = `Hodili jste číslo: ${hozeneCislo}`;
-    vysledky.appendChild(vysledek); 
-    /** sčítání **/ 
-   soucet += hozeneCislo;  
-   pocetHozeni += 1;
-   /**Hodili jste: */ 
-   pocetHodu.textContent = `Hodili jste: ${soucet}`; 
-   pocetBodu.textContent = `Počet hodů: ${pocetHozeni}`; 
-   /** podmínky výhry **/ 
-   if (pocetHozeni <= 4 && soucet >= 18) { 
-    const vyhra = document.createElement('h2'); 
-    vyhra.textContent = 'Vyhrál jsi!!'; 
+/*** fce na hod kostkou ***/
+const hodKostkou = (): number | void => {
+  const hozeneCislo: number = Math.floor(Math.random() * 6) + 1;
+  kostka.src = `./img/${hozeneCislo}.jpg`;
+  /** výsledky **/
+  const vysledek = document.createElement("p");
+  vysledek.classList.add("temporary");
+  vysledek.textContent = `Hodili jste číslo: ${hozeneCislo}`;
+  vysledky.appendChild(vysledek);
+  /** sčítání **/
+  soucet += hozeneCislo;
+  pocetHozeni += 1;
+  /**Hodili jste: */
+  pocetHodu.textContent = `Hodili jste: ${soucet}`;
+  pocetBodu.textContent = `Počet hodů: ${pocetHozeni}`;
+  /** podmínky výhry **/
+  if (pocetHozeni <= 4 && soucet >= 18) {
+    const vyhra = document.createElement("h2");
+    vyhra.textContent = "Vyhrál jsi!!";
 
-    vysledky.appendChild(vyhra); 
-    const deleteTemporary = document.querySelectorAll('.temporary'); 
+    vysledky.appendChild(vyhra);
+    const deleteTemporary = document.querySelectorAll(".temporary");
 
-    deleteTemporary.forEach(element => element.remove())
-   }
-} 
+    deleteTemporary.forEach((element) => element.remove());
+  } else if (pocetHozeni === 4 && soucet < 18) {
+    const prohra = document.createElement("h2");
+    prohra.textContent = "Prohrál jsi!!";
 
-const vynulujVysledek = () => { 
-    location.reload();
-}
+    vysledky.appendChild(prohra);
+    const deleteTemporary = document.querySelectorAll(".temporary");
 
-if (tlacitko && vynuluj){ 
-    tlacitko.addEventListener('click', hodKostkou); 
-    vynuluj.addEventListener('click', vynulujVysledek);
+    deleteTemporary.forEach((element) => element.remove());
+  }
+};
+
+const vynulujVysledek = () => {
+  location.reload();
+};
+
+if (tlacitko && vynuluj) {
+  tlacitko.addEventListener("click", hodKostkou);
+  vynuluj.addEventListener("click", vynulujVysledek);
 }
